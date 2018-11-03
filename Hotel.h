2e85d3ff7d78546b15edcca11171275712c3862f
NikSkalski <ludek97@o2.pl>
2018-11-03 22:55:21 +0100

    Initial commit

---
+++ Hotel.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Hotel
+{
+public:
+	Hotel(string HotelName, string City, int price) {
+		this->HotelName = HotelName;
+		this->City = City;
+		this->price = price;
+	}
+	~Hotel();
+	Hotel();
+
+
+
+	int Cost(int days) {
+		return days * price;
+	}
+	string getName() {
+		return HotelName;
+	}
+	string GetCity() {
+		return City;
+	}
+	void ToString() {
+		cout << "Name: " << HotelName << " City: " << City << endl;
+	}
+	void HotelFinder(vector <Hotel> ListOfHotels, string Place) {
+		for (int i = 0; i < ListOfHotels.size(); i++) { //iteruje po wszystkich obiektach w vektorze
+			if ((!ListOfHotels[i].GetCity().compare(Place))) {  //porównuje misto z wpisanym miastem
+				ListOfHotels[i].ToString();		//wywo³uje funkcje wypisuj¹c¹
+			}
+		}
+	}
+private:
+	string HotelName, City;
+	int price;
+
+};
+
